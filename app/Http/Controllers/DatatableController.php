5314f9589443454f42dd955b8609b9861067d44d
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2024-04-30 23:35:52 +0600

    CRUD with AJAX

--- app/Http/Controllers/DatatableController.php
+++ app/Http/Controllers/DatatableController.php
@@ -9,12 +9,21 @@ class DatatableController extends Controller
 {
     public function dataTableLogic(Request $request)
     {
-        if ($request->ajax()) {
-            $users = User::select('*');
-            return datatables()->of($users)
+        if($request->ajax()) {
+            return datatables()->of(User::select('*'))
+                ->addColumn('action', function($row){
+
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="View"  class="me-1 btn btn-info btn-sm showProduct"><i class="fa-regular fa-eye"></i> View</a>';
+                    $btn = $btn. '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" onClick="editFunc({{ $id }})"  class="edit btn btn-primary btn-sm editProduct"><i class="fa-regular fa-pen-to-square"></i> Edit</a>';
+
+                    $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct"><i class="fa-solid fa-trash"></i> Delete</a>';
+
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->addIndexColumn()
                 ->make(true);
         }
-
-        return view('y-dataTables');
+        return view('blogs');
     }
 }
